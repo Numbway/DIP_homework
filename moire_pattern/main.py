9b127dae41bdaf0e2aed40c8f156cbb35c6398e4
WeiJun <Wei_jun1225@outlook.com>
2024-11-21 14:15:51 +0800

    moire_pattern

--- moire_pattern/main.py
+++ moire_pattern/main.py
@@ -28,6 +28,6 @@ def remove_moire(image_path, notch_centers, radius):
 # 使用示例
 if __name__ == '__main__':
 
-    image_path = r'D:\File\Postgraduate\First_Year\Digital_Image_Procesing\DIP_homework\moire_pattern\image\car-moire-pattern.tif'  # 替换为你的图像路径
+    image_path = r'./image/car-moire-pattern.tif'  # 替换为你的图像路径
     notch_centers = [(205, 55), (205, 115), (165, 55), (165, 115), (80, 55), (80, 115), (40, 55), (40, 115)]
     remove_moire(image_path, notch_centers, radius=8)
